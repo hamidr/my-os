a902bfe6375da4be3715e1c3216d87af9a0069c4
hamidr <hamidr@gmail.com>
2025-10-21 17:52:09 +0200

    di 21 okt 2025 17:52:09 CEST

--- home/neovim.nix
+++ home/neovim.nix
@@ -3,8 +3,8 @@ let
   user = (import ../cfg.nix {}).user.username;
 in
 {
-  imports = [ lazyvim.homeManagerModules.default ];
   home-manager.users.${user}.programs.lazyvim = {
+  imports = [ lazyvim.homeManagerModules.default ];
     enable = true;
   };
 
