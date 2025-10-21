b723460b95df23c7c6bc8bfa08981053f6f3b250
hamidr <hamidr@gmail.com>
2025-10-21 17:50:26 +0200

    di 21 okt 2025 17:50:26 CEST

--- home/neovim.nix
+++ home/neovim.nix
@@ -3,29 +3,7 @@ let
   user = (import ../cfg.nix {}).user.username;
 in
 {
-  home-manager.users.${user}.programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    configure = {
-      customRC = ''
-        set nocompatible
-        set backspace=indent,eol,start
-        syntax on
-        set number
-        set expandtab
-        set tabstop=2
-        set shiftwidth=2
-      '';
-      packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [ ctrlp ];
-      };
-    };
-  };
-
   imports = [ lazyvim.homeManagerModules.default ];
-
   home-manager.users.${user}.programs.lazyvim = {
     enable = true;
   };
