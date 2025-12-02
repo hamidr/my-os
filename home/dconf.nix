ea019ddb4fec20ae875c80d65a194c71111425a9
hamidr <hamidr@gmail.com>
2025-12-02 17:13:33 +0100

    di  2 dec 2025 17:13:33 CET

--- home/dconf.nix
+++ home/dconf.nix
@@ -4,6 +4,8 @@ let
 in
 {
   home-manager.users.${user} = {
+    home.packages = with pkgs; [ dconf ];
+
     dconf = {
       enable = true;
       settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
