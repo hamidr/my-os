090a91635d832198a66193dca5ae8e6d97b36d59
hamidr <hamidr@gmail.com>
2025-12-02 17:16:28 +0100

    di  2 dec 2025 17:16:28 CET

--- cfg.nix
+++ cfg.nix
@@ -30,30 +30,8 @@ let
     font-mono-size = 17;
   };
 
-  colors = {
-    black = "26292D";
-    red = "ED1515";
-    green = "11D116";
-    yellow = "F67400";
-    blue = "1E92FF";
-    purple = "9B59B6";
-    cyan = "1ABC9C";
-    white = "EFF0F1";
-    bright_black = "4F5255";
-    bright_red = "C0392B";
-    bright_green = "1CDC9A";
-    bright_yellow = "FDBC4B";
-    bright_blue = "3DAEE9";
-    bright_purple = "8E44AD";
-    bright_cyan = "16A085";
-    bright_white = "FCFCFC";
-    background = "31363B";
-    foreground = "EFF0F1";
-  };
-
 in {
   inherit system;
   inherit user;
   inherit style;
-  inherit colors;
 }
