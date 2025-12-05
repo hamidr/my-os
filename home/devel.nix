800c3e70fc205990a64b9f39080d03c9bddc7c0e
hamidr <hamidr@gmail.com>
2025-12-05 12:21:22 +0100

    vr  5 dec 2025 12:21:22 CET

--- home/devel.nix
+++ home/devel.nix
@@ -5,19 +5,8 @@ in
 {
   home-manager.users.${user} = {
     home.packages = with pkgs; [
-      gcc
       pgcli
-      gnumake
-      cmake
       qtcreator
     ];
-
-    programs = {
-      go.enable = true;
-      java = {
-        enable = true;
-        package = pkgs.oraclejre22;
-      };
-    };
   };
 }
